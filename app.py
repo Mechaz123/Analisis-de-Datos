6a8cd9c156e7ce5e68df741876cef0389498c56b
Mechaz123 <danielalejandrorojasgonzalez@gmail.com>
2023-12-03 19:53:12 -0500

    feat: Se crean los campos necesarios para el funcionamiento.

--- app.py
+++ app.py
@@ -1,18 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 
 
 app = Flask(__name__)
 
 # RUTAS
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def raiz():
-    return render_template('index.html')
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        edad = request.form['edad']
+        tarifa_diaria = request.form['tarifa_diaria']
+        distancia_casa = request.form['distancia_casa']
+        nivel_educativo = request.form['nivel_educativo']
+        experiencia = request.form['experiencia']
+        balance = request.form['balance']
+        viajes_negocios = request.form['viajes_negocios']
+        dependencia = request.form['dependencia']
+        ambito_educativo = request.form['ambito_educativo']
 
-#ruta para nosotros
-@app.route('/base')
-def base():
-    return render_template('base.html')
+        #return render_template('resultado.html', nombre=nombre, edad=edad)
+    return render_template('index.html')
 
 #Bloque de prueba
 if __name__== "__main__":
-    app.run(debug=True)
+    app.run(debug = True)
